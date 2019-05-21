3d179579ec896b720a4ad6fa2a3a456e55823139
ArghirSergiu <arghirsergiu@gmail.com>
2019-05-21 21:29:46 +0300

    Added LearningVariables

---
+++ Lesson4/LearningVariables.java
@@ -0,0 +1,26 @@
+
+public class LearningVariables{
+	
+	public static void main (String [] args){
+	
+	 boolean SunnyDay= true;
+         
+         double OutsideTemp= 25.7;
+
+         int MediumTemp=24;
+    
+         String Meteo ="Today wheater";	
+
+         int Day,Month,Year;	
+         
+	 boolean Sun,Fog,Rain,Snow;
+         
+         String x,f,g;
+	
+	}
+}
+
+
+
+
+    
