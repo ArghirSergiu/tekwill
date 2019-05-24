c81e91b618fac25d7e990352e27959d7b8f8c55d
ArghirSergiu <arghirsergiu@gmail.com>
2019-05-24 13:27:43 +0300

    Added Homework Lesson5

---
+++ Lesson 5 -Variables/WorkWithType.java
@@ -0,0 +1,41 @@
+
+public class WorkWithType {
+
+              public static void main (String [] args){
+	 byte childAge,juniorAge,studentAge,workerAge,retireAge;
+                     childAge=3;
+                     juniorAge=10;
+                     studentAge=18;
+                     workerAge=30;
+      	 retireAge=68;
+                     short kinderGarden,school,university,jobs,retireHouses;
+                     kinderGarden=345;
+                     school= 640;
+                     university=98;
+                     jobs= 25540;
+                     retireHouses=210;
+        	 int cars=3507580;
+                     int trucks=1200450;
+                     int motorcycles=230025;
+                     int bicycles =680420;
+                     int buses =50240;
+                     long trees= 254786500045L;
+                     long bushes=621450125456L;
+                     long grassSquare=320457865417L;
+                     long starsOnSky=5542215262355415152l;
+                     long distanceToSaturnCm=1349463750000000000L;
+                     float EurUsd,GpbUsd,UsdChf,UsdJpy,UsdCad;
+                     EurUsd=1.119356487524517F;
+ 	 GpbUsd=1.271155697511212F;
+	 UsdChf=1.0256875121000145F;
+	 UsdJpy=109.675644215785125412F;
+	 UsdCad=1.345682110225545522211F;
+                     char Usd='$';
+                     char Pound='?';
+ 	 char Yen='?';
+                     char Euro='?';
+                     char Hryvnia='?';
+
+
+}
+}
