f8ff9850ba089259183e3543c1a0cf384e4f323f
ArghirSergiu <arghirsergiu@gmail.com>
2019-05-27 11:24:39 +0300

    Homework Lesson 6

---
+++ Lesson 6 -Conditionals/IfElse.java
@@ -0,0 +1,119 @@
+public class IfElse {
+    public static void main (String [] args) {
+        int a,s,d,f,g,h,j,k,l,z,x,c,v,b,n;
+		a=-14;
+		s= 0;
+		d= 23;
+		f= -3;
+		g= 8;
+		h= -4;
+		j= 0;
+		k= 36;
+		l= -129;
+		z= 234;
+		x= 65;
+		c=-88;
+		v= -501;
+		b=76;
+		n=0;
+		 System.out.println ("Verificarea variabilelor <> sau = 0 :");
+		 System.out.println("Variabila a este :"+a);
+        if (a>0){
+			System.out.println("Valoarea variabilei este mai mare ca 0 si este un numar par si se icadreaza in byte ");
+		}else if (a<0){
+			System.out.println("Valoarea variabilei este mai mica ca 0");
+		}else {	
+		 System.out.println("Valoarea variabilei este egala cu 0");
+		}
+		 System.out.println("Variabila s este :"+s);
+        if (s>0){
+			System.out.println("Valoarea variabilei este mai mare ca 0");
+		}else if (s<0){
+			System.out.println("Valoarea variabilei este mai mica ca 0");
+		} else {	
+		 System.out.println("Valoarea variabilei este egala cu 0");
+		} 
+		  System.out.println("Variabila d este :"+d);
+        if (d>0){
+			System.out.println("Valoarea variabilei este mai mare ca 0");
+		}else if (d<0){
+			System.out.println("Valoarea variabilei este mai mica ca 0");
+		}else {	
+		 System.out.println("Valoarea variabilei este egala cu 0");
+		}
+		System.out.println("Variabila f este :"+f);
+        if (f>0){
+			System.out.println("Valoarea variabilei este mai mare ca 0");
+		}else if (f<0){
+			System.out.println("Valoarea variabilei este mai mica ca 0");
+		}else {	
+		 System.out.println("Valoarea variabilei este egala cu 0");
+		}
+         System.out.println("Variabila g este :"+g);
+        if (g>0){
+			System.out.println("Valoarea variabilei este mai mare ca 0");
+		}else if (g<0){
+			System.out.println("Valoarea variabilei este mai mica ca 0");
+		}else {	
+		 System.out.println("Valoarea variabilei este egala cu 0");
+	}
+	     System.out.println("Variabila h este :"+h);
+        if (h>0){
+			System.out.println("Valoarea variabilei este mai mare ca 0");
+		}else if (h<0){
+			System.out.println("Valoarea variabilei este mai mica ca 0");
+		}else {	
+		 System.out.println("Valoarea variabilei este egala cu 0");
+	     
+		 System.out.println("Variabila j este :"+j);
+        if (j>0){
+			System.out.println("Valoarea variabilei este mai mare ca 0 si este un numar par si se icadreaza in byte ");
+		}else if (j<0){
+			System.out.println("Valoarea variabilei este mai mica ca 0");
+		}else {	
+		 System.out.println("Valoarea variabilei este egala cu 0");
+		}
+		 System.out.println("Variabila k este :"+k);
+        if (k>0){
+			System.out.println("Valoarea variabilei este mai mare ca 0");
+		}else if (k<0){
+			System.out.println("Valoarea variabilei este mai mica ca 0");
+		} else {	
+		 System.out.println("Valoarea variabilei este egala cu 0");
+		} 
+		  System.out.println("Variabila l este :"+l);
+        if (l>0){
+			System.out.println("Valoarea variabilei este mai mare ca 0");
+		}else if (l<0){
+			System.out.println("Valoarea variabilei este mai mica ca 0");
+		}else {	
+		 System.out.println("Valoarea variabilei este egala cu 0");
+		}
+		System.out.println("Variabila z este :"+z);
+        if (z>0){
+			System.out.println("Valoarea variabilei este mai mare ca 0");
+		}else if (z<0){
+			System.out.println("Valoarea variabilei este mai mica ca 0");
+		}else {	
+		 System.out.println("Valoarea variabilei este egala cu 0");
+		}
+         System.out.println("Variabila x este :"+x);
+        if (x>0){
+			System.out.println("Valoarea variabilei este mai mare ca 0");
+		}else if (x<0){
+			System.out.println("Valoarea variabilei este mai mica ca 0");
+		}else {	
+		 System.out.println("Valoarea variabilei este egala cu 0");
+	    }
+	     System.out.println("Variabila h este :"+c);
+        if (c>0){
+			System.out.println("Valoarea variabilei este mai mare ca 0");
+		}else if (c<0){
+			System.out.println("Valoarea variabilei este mai mica ca 0");
+		}else 
+		 System.out.println("Valoarea variabilei este egala cu 0");
+		 
+}
+}
+	
+}
