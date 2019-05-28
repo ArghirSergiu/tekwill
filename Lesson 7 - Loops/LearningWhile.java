0831d20fb10f6e4c2b3e8d858a886a870aa04893
ArghirSergiu <arghirsergiu@gmail.com>
2019-05-28 14:35:32 +0300

    Homework Lesson 7 'While'

---
+++ Lesson 7 - Loops/LearningWhile.java
@@ -0,0 +1,25 @@
+public class LearningWhile {
+    public static void main (String [] args){
+       int num  = 0, par =0, impar =50;
+	      System.out.println("____Enumararea de la 0 la 100:_____");
+       while (num <=100){
+           System.out.println (num);
+		num++;
+		}
+          System.out.println("____Numere pare interval 0-50 :_____");
+		  while(par<=50){
+			  par++;
+			  if(par%2==0){
+           System.out.println (par);
+		  }}
+			
+			  System.out.println("____Numere impare interval 50-100 :_____");
+		  while(impar<=99){
+			  impar++;
+			  if(impar%2==1){
+           System.out.println (impar);
+		  }}
+       
+    }
+
+}
