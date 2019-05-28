4665793984132dbdb9462cf0e3899051c7063c1d
ArghirSergiu <arghirsergiu@gmail.com>
2019-05-28 09:18:36 +0300

    Homework Lesson 6 finished

--- Lesson 6 -Conditionals/IfElse.java
+++ Lesson 6 -Conditionals/IfElse.java
@@ -1,13 +1,13 @@
 public class IfElse {
     public static void main (String [] args) {
         int a,s,d,f,g,h,j,k,l,z,x,c,v,b,n;
-		a=-14;
+		a=-18;
 		s= 0;
 		d= 23;
 		f= -3;
 		g= 8;
 		h= -4;
-		j= 0;
+		j= 1;
 		k= 36;
 		l= -129;
 		z= 234;
@@ -16,7 +16,8 @@ public class IfElse {
 		v= -501;
 		b=76;
 		n=0;
-		 System.out.println ("Verificarea variabilelor <> sau = 0 :");
+		String parImpar;
+		 System.out.println ("_____2.1______Verificarea variabilelor <> sau = 0_____________ :");
 		 System.out.println("Variabila a este :"+a);
         if (a>0){
 			System.out.println("Valoarea variabilei este mai mare ca 0 si este un numar par si se icadreaza in byte ");
@@ -65,9 +66,10 @@ public class IfElse {
 		}else {	
 		 System.out.println("Valoarea variabilei este egala cu 0");
 	     
+		}
 		 System.out.println("Variabila j este :"+j);
         if (j>0){
-			System.out.println("Valoarea variabilei este mai mare ca 0 si este un numar par si se icadreaza in byte ");
+			System.out.println("Valoarea variabilei este mai mare ca 0  ");
 		}else if (j<0){
 			System.out.println("Valoarea variabilei este mai mica ca 0");
 		}else {	
@@ -105,15 +107,160 @@ public class IfElse {
 		}else {	
 		 System.out.println("Valoarea variabilei este egala cu 0");
 	    }
-	     System.out.println("Variabila h este :"+c);
+	     System.out.println("Variabila c este :"+c);
         if (c>0){
 			System.out.println("Valoarea variabilei este mai mare ca 0");
 		}else if (c<0){
 			System.out.println("Valoarea variabilei este mai mica ca 0");
-		}else 
+		}else {
 		 System.out.println("Valoarea variabilei este egala cu 0");
-		 
-}
+		}
+		System.out.println("Variabila v este :"+v);
+        if (v>0){
+			System.out.println("Valoarea variabilei este mai mare ca 0");
+		}else if (v<0){
+			System.out.println("Valoarea variabilei este mai mica ca 0");
+		}else {	
+		 System.out.println("Valoarea variabilei este egala cu 0");
+		}
+         System.out.println("Variabila b este :"+b);
+        if (b>0){
+			System.out.println("Valoarea variabilei este mai mare ca 0");
+		}else if (b<0){
+			System.out.println("Valoarea variabilei este mai mica ca 0");
+		}else {	
+		 System.out.println("Valoarea variabilei este egala cu 0");
+	    }
+	     System.out.println("Variabila n este :"+n);
+        if (n>0){
+			System.out.println("Valoarea variabilei este mai mare ca 0");
+		}else if (n<0){
+			System.out.println("Valoarea variabilei este mai mica ca 0");
+		}else {
+		 System.out.println("Valoarea variabilei este egala cu 0");
+		}
+        System.out.println ("____2.2_____Verificarea par/impar______________:");
+		parImpar = a%2==0 ? "Numarul a  este par":"Numarul a este impar";
+		System.out.println (a);
+		System.out.println ( parImpar);
+		parImpar = s%2==0 ? "Numarul s  este par":"Numarul s este impar";
+		System.out.println (s);
+		System.out.println ( parImpar);
+		parImpar = d%2==0 ? "Numarul d  este par":"Numarul d este impar";
+		System.out.println (d);
+		System.out.println ( parImpar);
+		parImpar = f%2==0 ? "Numarul f este par":"Numarul f este impar";
+		System.out.println (f);
+		System.out.println ( parImpar);
+		parImpar = g%2==0 ? "Numarul g  este par":"Numarul g este impar";
+		System.out.println (g);
+		System.out.println ( parImpar);
+		parImpar = h%2==0 ? "Numarul h  este par":"Numarul h este impar";
+		System.out.println (h);
+		System.out.println ( parImpar);
+		parImpar = j%2==0 ? "Numarul j  este par":"Numarul j este impar";
+		System.out.println (j);
+		System.out.println ( parImpar);
+		parImpar = k%2==0 ? "Numarul k este par":"Numarul k este impar";
+		System.out.println (k);
+		System.out.println ( parImpar);
+		parImpar = l%2==0 ? "Numarul l  este par":"Numarul l este impar";
+		System.out.println (l);
+		System.out.println ( parImpar);
+		parImpar = z%2==0 ? "Numarul z  este par":"Numarul z este impar";
+		System.out.println (z);
+		System.out.println ( parImpar);
+		parImpar = x%2==0 ? "Numarul x  este par":"Numarul x este impar";
+		System.out.println (x);
+		System.out.println ( parImpar);
+		parImpar = c%2==0 ? "Numarul c  este par":"Numarul c este impar";
+		System.out.println (c);
+		System.out.println ( parImpar);
+		parImpar = v%2==0 ? "Numarul v  este par":"Numarul v este impar";
+		System.out.println (v);
+		System.out.println ( parImpar);
+		parImpar = b%2==0 ? "Numarul b  este par":"Numarul b este impar";
+		System.out.println (b);
+		System.out.println ( parImpar);
+		parImpar = n%2==0 ? "Numarul n este par":"Numarul n este impar";
+		System.out.println (n);
+		System.out.println ( parImpar);
+		System.out.println ("____2.3_____Incadrarea numerelor in byte_____________:");
+		if (a>=-127&&a<=128){
+			System.out.println ( "Numarul a se incadreaza in byte   "+a);
+	     }else {
+			 System.out.println ("Numarul a nu se incadreaza in byte  "+a);
+		 }
+		 if (s>=-127&&s<=128){
+			System.out.println ( "Numarul s se incadreaza in byte   "+s);
+	     }else {
+			 System.out.println ("Numarul s nu se incadreaza in byte  "+s);
+		 }
+		 if (d>=-127&&d<=128){
+			System.out.println ( "Numarul d se incadreaza in byte   "+d);
+	     }else {
+			 System.out.println ("Numarul d nu se incadreaza in byte  "+d);
+		 }
+		 if (f>=-127&&f<=128){
+			System.out.println ( "Numarul f se incadreaza in byte   "+f);
+	     }else {
+			 System.out.println ("Numarul f nu se incadreaza in byte  "+f);
+		 }
+		 if (g>=-127&&g<=128){
+			System.out.println ( "Numarul g se incadreaza in byte   "+g);
+	     }else {
+			 System.out.println ("Numarul g nu se incadreaza in byte  "+g);
+		 }
+		 if (h>=-127&&h<=128){
+			System.out.println ( "Numarul h se incadreaza in byte   "+h);
+	     }else {
+			 System.out.println ("Numarul h nu se incadreaza in byte  "+h);
+		 }
+		 if (j>=-127&&j<=128){
+			System.out.println ( "Numarul j se incadreaza in byte   "+j);
+	     }else {
+			 System.out.println ("Numarul j nu se incadreaza in byte  "+j);
+		 }
+		 if (k>=-127&&k<=128){
+			System.out.println ( "Numarul k se incadreaza in byte   "+k);
+	     }else {
+			 System.out.println ("Numarul k nu se incadreaza in byte  "+k);
+		 }
+		 if (l>=-127&&l<=128){
+			System.out.println ( "Numarul l se incadreaza in byte   "+l);
+	     }else {
+			 System.out.println ("Numarul l nu se incadreaza in byte  "+l);
+		 }
+		 if (z>=-127&&z<=128){
+			System.out.println ( "Numarul z se incadreaza in byte   "+z);
+	     }else {
+			 System.out.println ("Numarul z nu se incadreaza in byte  "+z);
+		 }
+		 if (x>=-127&&x<=128){
+			System.out.println ( "Numarul x se incadreaza in byte   "+x);
+	     }else {
+			 System.out.println ("Numarul x nu se incadreaza in byte  "+x);
+		 }
+		 if (c>=-127&&c<=128){
+			System.out.println ( "Numarul c se incadreaza in byte   "+c);
+	     }else {
+			 System.out.println ("Numarul c nu se incadreaza in byte  "+c);
+		 }
+		 if (v>=-127&&v<=128){
+			System.out.println ( "Numarul v se incadreaza in byte   "+v);
+	     }else {
+			 System.out.println ("Numarul v nu se incadreaza in byte  "+v);
+		 }
+		 if (b>=-127&&b<=128){
+			System.out.println ( "Numarul b se incadreaza in byte   "+b);
+	     }else {
+			 System.out.println ("Numarul b nu se incadreaza in byte  "+b);
+		 }
+		 if (n>=-127&&n<=128){
+			System.out.println ( "Numarul n se incadreaza in byte   "+n);
+	     }else {
+			 System.out.println ("Numarul n nu se incadreaza in byte  "+n);
+		 }
 }
 	
 }
