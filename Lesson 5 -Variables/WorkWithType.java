5384ea0a758bf2be07b2c5563d4cceb60793790d
ArghirSergiu <arghirsergiu@gmail.com>
2019-06-03 11:15:58 +0300

    Corectat char adaugat double

--- Lesson 5 -Variables/WorkWithType.java
+++ Lesson 5 -Variables/WorkWithType.java
@@ -7,14 +7,14 @@ public class WorkWithType {
                      juniorAge=10;
                      studentAge=18;
                      workerAge=30;
-      	 retireAge=68;
+      	             retireAge=68;
                      short kinderGarden,school,university,jobs,retireHouses;
                      kinderGarden=345;
                      school= 640;
                      university=98;
                      jobs= 25540;
                      retireHouses=210;
-        	 int cars=3507580;
+        	            int cars=3507580;
                      int trucks=1200450;
                      int motorcycles=230025;
                      int bicycles =680420;
@@ -24,17 +24,20 @@ public class WorkWithType {
                      long grassSquare=320457865417L;
                      long starsOnSky=5542215262355415152l;
                      long distanceToSaturnCm=1349463750000000000L;
-                     float EurUsd,GpbUsd,UsdChf,UsdJpy,UsdCad;
-                     EurUsd=1.119356487524517F;
- 	 GpbUsd=1.271155697511212F;
-	 UsdChf=1.0256875121000145F;
-	 UsdJpy=109.675644215785125412F;
-	 UsdCad=1.345682110225545522211F;
-                     char Usd='$';
-                     char Pound='?';
- 	 char Yen='?';
-                     char Euro='?';
-                     char Hryvnia='?';
+                     float eurUsd,gpbUsd,usdChf,usdJpy,usdCad;
+                     eurUsd=1.119356487524517F;
+					      gpbUsd=1.271155697511212F;
+                     char usd ='U';
+                     usdChf=1.0256875121000145F;
+                     usdJpy=109.675644215785125412F;
+                     usdCad=1.345682110225545522211F;
+                     char pound='P';
+                     char euro= 'E';
+                     char hryvnia='H';
+                     double pi = 3.14159265359;
+                     double razaPamintEcuator =6371.6;
+                     double razaPamintPolar = 6356.8;
+
 
 
 }
