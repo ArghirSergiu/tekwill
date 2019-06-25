52df755299a945c9b01f2c73eefcb05be74176d7
ArghirSergiu <arghirsergiu@gmail.com>
2019-06-25 16:05:43 +0300

    Calculator Homework

---
+++ Lesson 16-Overloaded/Calculator.java
@@ -0,0 +1,61 @@
+
+public class Calculator {
+    
+  public double dsuma (double a ,double b){
+       return a+b;
+   }
+   public double dscadere(double a, double b){
+       return a-b;
+   }
+   public double dinmultire  (double a, double b){
+       return a * b;
+   }
+   public double dimpartire (double a,double b){
+        return a / b;
+   }
+    public int isuma (int a ,int b){
+       return a+b;
+   }
+   public int iscadere(int a, int b){
+       return a-b;
+   }
+   public int iinmultire  (int a, int b){
+       return a * b;
+   }
+   public int iimpartire (int a,int b){
+        return a / b;
+   }
+   public float fsuma (float a ,float b){
+       return a+b;
+   }
+   public float fscadere(float a, float b){
+       return a-b;
+   }
+   public float finmultire  (float a, float b){
+       return a * b;
+   }
+   public float fimpartire (float a,float b){
+        return a / b;
+   }
+   public long lsuma (long a ,long b){
+       return a+b;
+   }
+   public long lscadere(long a, long b){
+       return a-b;
+   }
+   public long linmultire  (long a, long b){
+       return a * b;
+   }
+   public long limpartire (long a,long b){
+        return a / b;
+   }
+   public double procentDouble(double a,double x){
+           return a-(x*100/a);
+}
+   public int procentInt(int a,int x){
+           return a-(x*100/a);
+}
+   public byte procentByte(byte a,byte x){
+           return(byte) (a-(x*100/a));
+   }
+}
